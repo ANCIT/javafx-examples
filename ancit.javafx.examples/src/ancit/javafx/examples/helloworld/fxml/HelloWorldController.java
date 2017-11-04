6a250b1e396d043976609dc598a1b658c0947761
Annamalai C <annamalai.chockalingam@gmail.com>
2017-11-04 16:29:27 +0530

    Generating the UI of JAVAFx Application using a Visual Tool called as SceneBuilder which generates a FXML

---
+++ ancit.javafx.examples/src/ancit/javafx/examples/helloworld/fxml/HelloWorldController.java
@@ -0,0 +1,41 @@
+package ancit.javafx.examples.helloworld.fxml;
+
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class HelloWorldController implements Initializable {
+
+	@FXML
+	private Button myButton;
+
+	@FXML
+	private TextField myTextField;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	// When user click on myButton
+	// this method will be called.
+	public void showDateTime(ActionEvent event) {
+	System.out.println("Button Clicked!");
+	Date now= new Date();
+	DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS");
+	// Model Data
+	String dateTimeString = df.format(now);
+	// Show in VIEW
+	myTextField.setText(dateTimeString);
+	}
+
+}
